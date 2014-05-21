59783443c08b84c62bf7204ceb4eea85fec41f54
sitha93 <lillesith@gmail.com>
2014-05-21 17:07:59 +0200

    laget akselerasjon, tror det skal funke

--- Prosjekt_Programmering_3/awsmSeeSharpGame/Classes/Accelration.cs
+++ Prosjekt_Programmering_3/awsmSeeSharpGame/Classes/Accelration.cs
@@ -13,23 +13,31 @@ namespace awsmSeeSharpGame.Classes
         public float thrustX { get; set; }
         public float thrustY { get; set; }
         public float rotationRad { get; set; }
+        public float accelerationY { get; set; }
 
         public Acceleration()
         {
             gravitationalAcceleration = 0.01635f; //635f;
         }
 
-        public Acceleration(float _gravity, float _thrust, float _rotation)
+        public Acceleration(float _gravity)
         {
             gravitationalAcceleration = 0.01635f;
+
+
          //   gravitationalAcceleration = _gravity;
 
+        //    thrust = _thrust;
+
+        }
+
+        public void SetAccelleration(float _thrust, float _rotation)
+        {
             thrust = _thrust;
             rotationRad = (_rotation / 180) * (float)Math.PI;
             thrustX = (thrust * (float)Math.Sin(rotationRad));
             thrustY = (thrust * (float)Math.Cos(rotationRad));
+            accelerationY = gravitationalAcceleration + thrustY;
         }
-
-
     }
 }
