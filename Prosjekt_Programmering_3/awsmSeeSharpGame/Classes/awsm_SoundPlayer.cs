c97b59603c8e79e8fa8d1f105b4b9837f9d83cfa
gerr8ard <pskogsru88@hotmail.com>
2014-05-21 13:19:12 +0200

    Sanger und shit

---
+++ Prosjekt_Programmering_3/awsmSeeSharpGame/Classes/awsm_SoundPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+
+namespace awsmSeeSharpGame.Classes
+{
+    public class awsm_SoundPlayer
+    {  
+        
+        private IWavePlayer waveOutDevice;
+        private AudioFileReader audioFileReader;
+        private string resourceUrl = System.Windows.Forms.Application.StartupPath + "\\Resources\\";
+
+        public awsm_SoundPlayer(string _soundName)
+        {
+             waveOutDevice = new WaveOut();
+
+             audioFileReader = new AudioFileReader(resourceUrl + _soundName);
+
+            waveOutDevice.Init(audioFileReader);
+
+            waveOutDevice.Play();
+        }
+
+        public void Stop()
+        {
+            waveOutDevice.Stop();
+        }
+        
+    }
+}
